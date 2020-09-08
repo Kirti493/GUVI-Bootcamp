53d5b4ae99de9e9237e68f516e52db3aabeb8446
Kirti493 <70234037+Kirti493@users.noreply.github.com>
2020-09-09 03:36:30 +0530

    Add files via upload

---
+++ Day1/dataType.js
@@ -0,0 +1,15 @@
+// Use typeof in all datatypes and check the result
+
+findDataType = () => {
+    console.log(typeof(1))
+    console.log(typeof(1.1))
+    console.log(typeof('1.1'))
+    console.log(typeof(true))
+    console.log(typeof(null))
+    console.log(typeof(undefined))
+    console.log(typeof([]))
+    console.log(typeof({}))
+
+
+}
+findDataType()
